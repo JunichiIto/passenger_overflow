635af52103e4c8c79db7eb9fb3b2d64ae5b3d198
Junichi Ito <june.1st.717@gmail.com>
2012-01-16 05:32:05 +0900

    Initial commit. #1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PassengerOverflow::Application.config.secret_token = '2aab8fec6669d63a3d3f50ab125cb1ce19fd21e9db64556a68420d23ba21befdfe0b42fc4529b72e318df1aa8b2366ebf1b31843901055f3b91d696fcc981c08'
